404da449204fd5ada596c98e436722b189e7c678
Rachmat Adi Prakoso <insaneadinesia@insaneadinesia.local>
2020-03-31 22:27:12 +0700

    create table tournaments & tournament_brackets

---
+++ database/migrations/2020_03_31_130632_create_tournament_brackets_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTournamentBracketsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tournament_brackets', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('tournament_id');
+            $table->integer('match_no');
+            $table->string('team_a');
+            $table->string('team_b');
+            $table->text('description');
+            $table->timestamps();
+
+            $table->foreign('tournament_id')->references('id')->on('tournaments');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tournament_brackets');
+    }
+}
